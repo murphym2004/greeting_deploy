2430f194b1f374f95b98287d9c7b431d50aa14d9
matthew <g00430349@atu.ie>
2025-11-28 17:35:12 +0000

    .env and compose

---
+++ src/main/java/atu/ie/greetingdeploy2/GreetingDeploy2Application.java
@@ -0,0 +1,13 @@
+package atu.ie.greetingdeploy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingDeploy2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingDeploy2Application.class, args);
+    }
+
+}
---
+++ src/test/java/atu/ie/greetingdeploy2/GreetingDeploy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.greetingdeploy2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingDeploy2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
